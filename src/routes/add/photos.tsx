a318c70488321563644efdc313502a45d49cee35
bkawk <email.will.in.china@gmail.com>
2020-01-02 23:07:12 +0800

    photo sort and filter functions

--- src/routes/add/photos.tsx
+++ src/routes/add/photos.tsx
@@ -1,6 +1,65 @@
 import React from 'react';
 
 const Photos: React.FC = () => {
+
+  // TODO: onload get the dtata from the api and pipe it into body, add alternating background colors
+  const data = [
+    { category: 'Double Deckers', busNumber: 102, country: 'United Kingdom', published: true },
+    { category: 'Double Deckers', busNumber: 973, country: 'United Kingdom', published: true },
+    { category: 'Double Deckers', busNumber: 814, country: 'United Kingdom', published: true },
+    { category: 'Double Deckers', busNumber: 7734, country: 'United Kingdom', published: true },
+    { category: 'Single Deckers', busNumber: 104, country: 'China', published: true },
+    { category: 'Single Deckers', busNumber: 496, country: 'China', published: true },
+    { category: 'Single Deckers', busNumber: 80, country: 'China', published: true },
+    { category: 'Single Deckers', busNumber: 712, country: 'China', published: true },
+  ];
+
+  interface CountProps {
+    category: string;
+    busNumber: number;
+    country: string;
+    published: boolean;
+  }
+
+  const count = (dataToCount: CountProps[]) => {
+    return dataToCount.reduce(
+      (reducedData: Array<{ category: string; total: number }>, v) => {
+        const f = reducedData.find((i) => i.category === v.category);
+        if (f) f.total++;
+        else reducedData.push({ category: v.category, total: 1 });
+        return reducedData;
+      },
+      []
+    );
+  };
+  // const summary = count(data);
+
+  const sort = (
+    dataToSort: CountProps[],
+    onlyShow: string,
+    sortBy: keyof CountProps,
+    ascDesc: -1 | 1
+  ) => {
+    if (onlyShow !== 'any') {
+      dataToSort
+        .filter((v) => v.category === onlyShow)
+        .sort((a, b) => {
+          let aValue = a[sortBy];
+          let bValue = b[sortBy];
+          if (typeof aValue === 'string') {
+            aValue = aValue.toLocaleUpperCase();
+          }
+          if (typeof bValue === 'string') {
+            bValue = bValue.toLocaleUpperCase();
+          }
+          if (aValue < bValue) return ascDesc;
+          if (aValue > bValue) return ascDesc * -1;
+          return 0;
+        });
+    }
+  };
+  // const sortedData = sort(data, 'Double Deckers', 'country', -1);
+
   return (
     <div className='photos'>
       <div className='grid'>
