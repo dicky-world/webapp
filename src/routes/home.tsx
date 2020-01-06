2461964d144606a87b4dea4396dc6ea126d8fa67
bkawk <email.will.in.china@gmail.com>
2020-01-06 19:34:34 +0800

    home page photos

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,12 +1,61 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Carousel } from '../components/carousel';
 import { Focus } from '../components/focus';
+import { Global } from '../globalState';
+import { AllPhotos } from '../utils/allPhotos';
 
 const Home: React.FC = () => {
+  const { global } = useContext(Global);
+
+  interface DataProps {
+    category: string;
+    published: boolean;
+    previewId: string;
+    thumbnailId: string;
+  }
+  interface StateInterface {
+    array: DataProps[];
+  }
+  const [photos, setPhotos] = useState<StateInterface>({
+    array: [],
+  });
+  const bgImage = (imageUrl: string) => {
+    return {
+      backgroundImage: `url(${imageUrl})`,
+      backgroundPosition: 'center',
+      backgroundSize: 'cover',
+      paddingBottom: '100%',
+    };
+  };
+  const getRows = () => {
+    const arr = [];
+    for (let i = 0; i <= photos.array.length; i++) {
+      if (photos.array[i]) {
+        arr.push(
+          <React.Fragment>
+            <div className='profile--image'
+              style={bgImage(global.env.imgUrl + photos.array[i].previewId)}
+            ></div>
+          </React.Fragment>
+        );
+      }
+    }
+    return arr;
+  };
+
+  useEffect(() => {
+    const getAllPhotos = async () => {
+      const content = await AllPhotos(`${global.env.apiUrl}/photos?limit=50&sortBy=newest`);
+      setPhotos(() => ({ array: content.photos }));
+    };
+    getAllPhotos();
+  }, []);
+
   return (
     <section>
       <Carousel />
       <Focus />
+      <div className='profile--image-grid'>{getRows()}</div>
     </section>
   );
 };
---
+++ src/utils/allPhotos.tsx
@@ -0,0 +1,21 @@
+const AllPhotos = async (apiEndpoint: string) => {
+  try {
+    const response = await fetch(apiEndpoint, {
+      headers: {
+        // prettier-ignore
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      method: 'GET',
+    });
+    const content = await response.json();
+    if (response.status === 200) {
+      return content;
+    }
+    throw Error;
+  } catch (error) {
+    return error;
+  }
+};
+
+export { AllPhotos };
